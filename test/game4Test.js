7d142d8862dff2cf21ffad135d4433bae1a2ab2c
AnsarQureshi1 <qureshiansar893@gmail.com>
2023-08-09 09:20:55 +0500

    first commit

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,19 +1,24 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game4', function () {
+  
   async function deployContractAndSetVariables() {
+    const addr1= ethers.provider.getSigner(0);
+    const addr2= ethers.provider.getSigner(0);
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    return { game ,addr1,addr2};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game , addr1,addr2 } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
-
-    await game.win();
+    await game.connect(addr1).write(addr2.getAddress());
+    await game.connect(addr2).win(addr1.getAddress())
+    
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
